a26bf00cfb57c68d57a85d34d96a01a1819ef75e
HeerMistry2312 <heermistry2312@gmail.com>
2024-04-13 15:31:34 +0530

    Part -16 & 17 Completed

--- BloodDonationApp/Controllers/RegistrationController.cs
+++ BloodDonationApp/Controllers/RegistrationController.cs
@@ -1,6 +1,8 @@
 ﻿using BloodDonationApp.Models;
+using DatabaseLayer;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,12 +11,119 @@ namespace BloodDonationApp.Controllers
 {
     public class RegistrationController : Controller
     {
-
-        public ActionResult UserRegistration()
+        ByteBridgeDbEntities DB = new ByteBridgeDbEntities();
+        static RegistrationMV registrationmv;
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SelectUser(RegistrationMV registrationMV)
         {
-            var registration = new RegistrationMV();
+            registrationmv = registrationMV;
+            if(registrationMV.UserTypeID == 2)
+            {
+                return RedirectToAction("DonorUser");
+            }else if (registrationMV.UserTypeID == 3)
+            {
+                return RedirectToAction("SeekerUser");
+            }
+            else if (registrationMV.UserTypeID == 4)
+            {
+                return RedirectToAction("HospitalUser");
+            }
+            else if (registrationMV.UserTypeID == 5)
+            {
+                return RedirectToAction("BloodBankUser");
+            }
+            else
+            {
+                return RedirectToAction("MainHome", "Home");
+            }
+            
+        }
 
-            return View(registration);
+        public ActionResult HospitalUser()
+        {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HospitalUser(RegistrationMV registrationMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var checktitle = DB.HospitalTables.Where(h => h.FullNAme == registrationMV.Hospital.FullNAme.Trim()).FirstOrDefault();
+                if(checktitle == null)
+                {
+                    var user = new UserTable();
+                    user.UserName = registrationMV.User.UserName;
+                    user.Password = registrationMV.User.Password;
+                    user.EmailAddress = registrationMV.User.EmailAddress;
+                    user.AccountStatusID = 1;
+                    user.UserTypeID = registrationMV.UserTypeID;
+                    user.Description = registrationMV.User.Description;
+                    DB.UserTables.Add(user);
+
+                    var hospital = new HospitalTable();
+                    hospital.FullNAme = registrationMV.Hospital.FullNAme;
+                    hospital.Address = registrationMV.Hospital.Address;
+                    hospital.Location = registrationMV.Hospital.Address;
+                    hospital.PhoneNo = registrationMV.Hospital.PhoneNo;
+                    hospital.Website = registrationMV.Hospital.Website;
+                    hospital.Email = registrationMV.Hospital.Email;
+                    hospital.CityID = registrationMV.CityID;
+                    hospital.UserID = user.UserID;
+                    DB.HospitalTables.Add(hospital);
+                   
+                        DB.SaveChanges();
+                    
+                    return RedirectToAction("MainHome", "Home");
+
+
+                }
+            }
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationMV.CityID);
+            return View(registrationMV);
+        }
+
+        public ActionResult DonorUser()
+        {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DonorUser(RegistrationMV registrationMV)
+        {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View();
+        }
+
+        public ActionResult BloodBankUser()
+        {
+            ViewBag.UserTypeID = new SelectList(DB.UserTypeTables.Where(ut => ut.UserTypeID > 1).ToList(), "UserTypeID", "UserType", registrationmv.UserTypeID);
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult BloodBankUser(RegistrationMV registrationMV)
+        {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View();
+        }
+
+        public ActionResult SeekerUser()
+        {
+           ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SeekerUser(RegistrationMV registrationMV)
+        {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View();
         }
     }
 }
