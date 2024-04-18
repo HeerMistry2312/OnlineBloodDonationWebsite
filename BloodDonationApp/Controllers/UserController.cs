b64fbee4c55c6dca5eb61c54cb618644e028df35
HeerMistry2312 <heermistry2312@gmail.com>
2024-04-18 08:18:57 +0530

    Part - 30 Completed

---
+++ BloodDonationApp/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BloodDonationApp.Controllers
+{
+    public class UserController : Controller
+    {
+        ByteBridgeDbEntities DB = new ByteBridgeDbEntities();
+        // GET: User
+        public ActionResult UserProfile(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var user = DB.UserTables.Find(id);
+            return View(user);
+        }
+    }
+}
